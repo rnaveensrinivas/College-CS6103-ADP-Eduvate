05f30a33bf9de7ba42c523cd5fa7e1e82ef40943
rnaveensrinivas <rnaveensrinivas@gmail.com>
2021-06-28 20:57:54 +0530

    Captcha done

---
+++ jsfunctions.js
@@ -0,0 +1,42 @@
+function newcaptcha(){ 
+    var alphabets = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'.split('');
+
+    var a = alphabets[ Math.floor(Math.random()* 62) ];
+    var b = alphabets[ Math.floor(Math.random()* 62) ]; 
+    var c = alphabets[ Math.floor(Math.random()* 62) ]; 
+    var d = alphabets[ Math.floor(Math.random()* 62) ]; 
+    var e = alphabets[ Math.floor(Math.random()* 62) ];
+    var f = alphabets[ Math.floor(Math.random()* 62) ];
+
+    
+    var captcha = a + b + c + d + e + f; 
+
+    document.getElementById('captcha').value = captcha ; 
+     
+}
+
+function validcap(){ 
+    var captcha = document.getElementById('captcha').value ; 
+    
+    var enteredCaptcha = document.getElementById('enteredCaptcha').value ; 
+
+    if( !enteredCaptcha ){ 
+        alert("Enter the captcha.") ; 
+    }
+    else if( captcha != enteredCaptcha ){ 
+        alert("Wrong captcha Try again.") ; 
+        return false ; 
+    }
+}
+
+function checkPassword() {
+    var p1 = document.getElementById("pwd1").value;
+    var p2 = document.getElementById("pwd2").value;
+
+    if(p1 != p2) {
+        alert("Check your password again!");
+    }
+    else {
+        alert("Details have been submitted successfully!");
+    }
+}
