608df7659a83853543519e2b0c516adfb1ef756d
Lakshana <lakshravi23@gmail.com>
2021-07-03 10:37:37 +0530

    added comments to script.js and server.js

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ const messageInput = document.getElementById('message-input')
 const personName = prompt('What is your name?')
 appendMessage('You joined')
 
-socket.emit('new-user', personName)
+socket.emit('new-user', personName)//shows this on the pther person's page
 
 
 socket.on('chat-message', data => {
     appendMessage(`${data.personName}: ${data.message}`)
-})
+}) //appends the name and the message sent by the person
 
 socket.on('user-connected', personName => {
-    appendMessage(`${personName} connected`)
+    appendMessage(`${personName} connected`) //shows person connected
 })
 socket.on('user-disconnected', personName => {
     appendMessage(`${personName} disconnected`)
@@ -24,11 +24,12 @@ socket.on('user-disconnected', personName => {
 
 
 messageForm.addEventListener('submit' , e => {
-    e.preventDefault()
+    e.preventDefault()//prevents refreshing of the page while clicking send button; otherwise messages 
+    //will be lost every time user clicks button
     const message = messageInput.value
-    appendMessage(`You: ${message}`)
+    appendMessage(`You: ${message}`)//whatever message you sent
     socket.emit('send-chat-message', message)
-    messageInput.value =''
+    messageInput.value =''//declaring it to empty string for next use
 })
 
 function appendMessage(message) {
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 //server side js
-//const io = require('socket.io')(3000)
+
 const io = require("socket.io")(3000, {
     cors: {
-      origin: "*",
+      origin: "*",//to overwrite the CORS policy
     },
   })
 
-  const users = {}
+  const users = {} //empty object
   io.on('connection', socket => {
     socket.on('new-user', personName => {
         users[socket.id] = personName
@@ -20,5 +20,8 @@ const io = require("socket.io")(3000, {
         socket.broadcast.emit('user-disconnected', users[socket.id])
         delete users[socket.id]
        
-    })
+    })//emit displays the text on the screen
+    //first declaring empty object users
+    //then the person's name is stored in the array
+    //and then once they disconnect, you delete their id from the array 
 })
