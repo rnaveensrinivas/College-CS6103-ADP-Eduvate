56bc5fbf9f527f15ff438c9299446f2e04449566
Lakshana <lakshravi23@gmail.com>
2021-07-11 09:49:31 +0530

    final changes to css

--- 3Chat/client/src/components/Join/Join.js
+++ 3Chat/client/src/components/Join/Join.js
@@ -8,7 +8,7 @@ const Join = () => {
     return (
         <div className="joinOuterContainer">
             <div className="joinInnerContainer">
-                <h1 className="heading">Join</h1>
+                <h1>Join</h1>
             <div>
             <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
         </div>
